f1fce1811129af5e53efd4106eea98143ed3fd9e
Jefferson Juarez <jefferson.juarez@galileo.edu>
2023-02-06 19:34:42 -0600

    first commit

---
+++ CalculadoraEstaciones.js
@@ -0,0 +1,46 @@
+var mes = Number(prompt("INGRESE NUMERO DE MES DEL 1 AL 12"))
+
+switch (mes) {
+  case 1:
+    window.alert("Mes: Enero - Estacion: INVIERNO")
+    break
+  case 2:
+    window.alert("Mes: Febrero - Estacion: INVIERNO")
+    break
+    case 3:
+    window.alert("Mes: Marzo - Estacion: PRIMAVERA")
+    break
+    case 4:
+    window.alert("Mes: Abril - Estacion: PRIMAVERA")
+    break
+    case 5:
+    window.alert("Mes: Mayo - Estacion: PRIMAVERA")
+    break
+    case 6:
+    window.alert("Mes: Junio - Estacion: VERANO")
+    break
+    case 7:
+    window.alert("Mes: Julio - Estacion: VERANO")
+    break
+    case 8:
+    window.alert("Mes: Agosto - Estacion: VERANO")
+    break
+    case 9:
+    window.alert("Mes: Septiembre - Estacion: OTOÑO")
+    break
+    case 10:
+    window.alert("Mes: Octubre - Estacion: OTOÑO")
+    break
+    case 11:
+    window.alert("Mes: Noviembre - Estacion: OTOÑO")
+    break
+    case 12:
+    window.alert("Mes: Diciembre - Estacion: INVIERNO")
+    break
+   
+    default:
+    window.alert("ESTE VALOR NO ES VALIDO")
+    
+    }
+var nombre
+window.alert(nombre = "Jefferson Juarez, carnet 23002744")
